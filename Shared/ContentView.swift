874c39b9c08f6d14cbf66d468a7d9cf23a42e0a1
Mike199503 <zth19950326@yahoo.com>
2021-01-04 20:12:05 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 朱桃禾 on 2021/1/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/WeiboDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiboDemoApp.swift
+//  Shared
+//
+//  Created by 朱桃禾 on 2021/1/4.
+//
+
+import SwiftUI
+
+@main
+struct WeiboDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
